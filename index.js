f36472ddde4a7664101cdf790c9defa2668cfc1d
Kalin Jack <kalinj1997@gmail.com>
2021-03-30 16:03:11 -0400

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const API_URL = "http://localhost:3000/Fruit"
+const API_URL = "http://localhost:3000/Fruit/"
 let fruitMenu = document.querySelector("div#fruit-menu")
 let fruitDetailImageDiv = document.querySelector("div#fruit-detail")
 let fruitDetailImageImg = fruitDetailImageDiv.querySelector("img")
 let fruitDetailImageName = fruitDetailImageDiv.querySelector("h2")
+let fruitForm = document.querySelector("form#fruit-rating")
+let fruitFormRating = fruitForm.querySelector("input#rating")
+let fruitFormComment = fruitForm.querySelector("textarea#comment")
 
+let displayFruit = {}
 
 // fetch API_URL, {
 //     method: 'GET'
@@ -25,6 +29,47 @@ fetch(API_URL)
         fruitImage.addEventListener("click", function(evt){
             fruitDetailImageImg.src = fruitObj.image
             fruitDetailImageName.innerText = fruitObj.name
+
+            fruitFormRating.value = fruitObj.ratings
+            fruitFormComment.innerText = fruitObj.comments
+
+            displayFruit = fruitObj
+
+
         })
     })
+})
+
+fruitForm.addEventListener("submit", function(evt){
+    evt.preventDefault()
+
+
+    let fruitId = displayFruit.id
+
+    let fruitRating = fruitFormRating.value
+
+
+    let fruitComment = fruitFormComment.value
+
+debugger
+
+    fetch (`http://localhost:3000/Fruit/${fruitId}`, { 
+    method: 'PATCH',
+    headers: {
+        "content-type": "application/json"
+
+    },
+    
+    body: JSON.stringify ({
+
+        ratings: fruitRating,
+        comments: fruitComment
+    })
+})
+    .then (res => res.json ())
+    .then (updatedfruitObj => {
+    displayFruit.rating = updatedfruitObj.ratings
+    displayFruit.comment = updatedfruitObj.comments
+
+})
 })
